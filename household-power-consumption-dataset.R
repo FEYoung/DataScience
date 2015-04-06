68cdb995261502f6ff89bc805fb2c5fd4d0c6e48
Fiona Young <lacilaci.feyoung@gmail.com>
2015-04-06 22:35:23 +0100

    dataset - household power consumption

--- household-power-consumption-dataset.R
+++ household-power-consumption-dataset.R
@@ -1,4 +1,9 @@
+UCI Machine Learning repository:
+
 https://archive.ics.uci.edu/ml/datasets/Individual+household+electric+power+consumption#
+
+
+Dataset download site:
 
 https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 
