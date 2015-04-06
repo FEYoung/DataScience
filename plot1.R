aa6c9fd4d03d127f51e47ba26a315a746406640c
Fiona Young <lacilaci.feyoung@gmail.com>
2015-04-06 22:14:01 +0100

    plot1.R

---
+++ plot1.R
@@ -0,0 +1,19 @@
+PLOT 1 DETAILS -
+
+Graph title :	Global Active Power
+
+y axis : 		Frequency
+y axis scale : 	0 - 1200
+
+x axis : 		Global Active Power (kilowatts)
+x axis scale :	0 - 6
+
+=======================================================================
+
+R script
+
+##loading libraries
+library(data.table)
+
+##reading the dataset into R
+
