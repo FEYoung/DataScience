7647b224971e4cfd59585377e40d387a8a7dddaa
Fiona Young <lacilaci.feyoung@gmail.com>
2016-08-11 11:09:20 +0100

    Create milestone-report.R

---
+++ milestone-report.R
@@ -0,0 +1,12 @@
+DATA SCIENCE SPECIALISM
+
+CAPSTONE PROJECT
+
+THE MILESTONE REPORT
+
+_____________________________________________
+
+The Milestone Report can be found at:
+
+http://rpubs.com/lacilaci/188833
+
