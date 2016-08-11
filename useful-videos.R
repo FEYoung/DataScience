70cb16a62b352a825cbc5f1ebcb00fce8ec920d9
Fiona Young <lacilaci.feyoung@gmail.com>
2016-08-11 19:09:56 +0100

    Create useful-videos.R

---
+++ useful-videos.R
@@ -0,0 +1,23 @@
+#NATURAL LANGUAGE PROGRAMMING
+
+##Professor Dan Jurafsky & Chris Manning
+
+------------------------------
+
+- VIDEO 1 - Introduction to N-grams @ https://www.youtube.com/watch?v=s3kKlUBa3b0
+
+- VIDEO 2 - Estimating N-gram Probabilities @ https://www.youtube.com/watch?v=o-CvoOkVrnY
+
+- VIDEO 3 - Evaluation and Perplexity @ https://www.youtube.com/watch?v=OHyVNCvnsTo
+
+- VIDEO 4 - Generalization and Zeros @ https://www.youtube.com/watch?v=s5Yg6qac9ag
+
+- VIDEO 5 - Smoothing: Add-One (Laplace Smoothing) @ https://www.youtube.com/watch?v=d8nVJjlMOYo
+
+- VIDEO 6 - Interpolation @ https://www.youtube.com/watch?v=-aMYz1tMfPg
+
+- VIDEO 7 - Good-Turing Smoothing @ https://www.youtube.com/watch?v=XdjCCkFUBKU
+
+- VIDEO 8 - Kneser-Ney Smoothing @ https://www.youtube.com/watch?v=wtB00EczoCM
+
+
