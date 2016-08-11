7c147c1a275ff75b0cb4f1d066059a617938baa8
Fiona Young <lacilaci.feyoung@gmail.com>
2016-08-11 11:06:50 +0100

    Update milestone-report-instructions.R

--- milestone-report-instructions.R
+++ milestone-report-instructions.R
@@ -2,11 +2,11 @@
 
 ##CAPSTONE PROJECT
 
-------------------------------------------------
+#========================================
 
 INSTRUCTIONS
 
-The goal of this project is just to display that you've gotten used to working with the data and that you are on 
+The goal of this project is just to display that you have gotten used to working with the data and that you are on 
 track to create your prediction algorithm. Please submit a report on R Pubs (http://rpubs.com/) that explains your
 exploratory analysis and your goals for the eventual app and algorithm. This document should be concise and explain 
 only the major features of the data you have identified and briefly summarize your plans for creating the prediction 
@@ -15,7 +15,7 @@ of tables and plots to illustrate important summaries of the data set.
 
 The motivation for this project is to: 
 
-1. Demonstrate that you've downloaded the data and have successfully loaded it in.
+1. Demonstrate that you have downloaded the data and have successfully loaded it in.
 2. Create a basic report of summary statistics about the data sets.
 3. Report any interesting findings that you amassed so far.
 4. Get feedback on your plans for creating a prediction algorithm and Shiny app.
@@ -23,13 +23,13 @@ The motivation for this project is to:
 
 REVIEWING OTHER STUDENTS WORK
 
-    Does the link lead to an HTML page describing the exploratory analysis of the training data set?
+1.  Does the link lead to an HTML page describing the exploratory analysis of the training data set?
     
-    Has the data scientist done basic summaries of the three files? Word counts, line counts and basic data tables?
+2.  Has the data scientist done basic summaries of the three files? Word counts, line counts and basic data tables?
     
-    Has the data scientist made basic plots, such as histograms to illustrate features of the data?
+3.  Has the data scientist made basic plots, such as histograms to illustrate features of the data?
     
-    Was the report written in a brief, concise style, in a way that a non-data scientist manager could appreciate?
+4.  Has the report written in a brief, concise style, in a way that a non-data scientist manager could appreciate?
     
     
     
